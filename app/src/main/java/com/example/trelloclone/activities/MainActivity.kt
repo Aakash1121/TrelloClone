d2ce1b5d0d4b057dbe745528b0a812a28764e7a3
Aakash1121 <80790275+Aakash1121@users.noreply.github.com>
2021-04-16 16:24:50 +0530

    hello commit

--- app/src/main/java/com/example/trelloclone/activities/MainActivity.kt
+++ app/src/main/java/com/example/trelloclone/activities/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         setupActionBar()
         nav_view.setNavigationItemSelectedListener(this)
         FireStoreClass().loadUserData(this)
-
+        println("Hello")
 
     }
 
